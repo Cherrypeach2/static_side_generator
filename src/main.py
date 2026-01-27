755c36d61d42be31a61d21f3c98e01237a515949
Cherrypeach2 <joshua.kauss21@gmail.com>
2026-01-27 18:00:33 +0100

    Block logic

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ from textnode import *
 from htmlnode import *
 
 def main(): 
-    print("Hello There")
-    node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
-    print(node.to_html())
     
+    return
+
+   
+def copy_static():
+    return 
     
     
 if __name__ == "__main__":
