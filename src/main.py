5516fa99e58deb1e8bf5db99736ba766f97e800a
Cherrypeach <joshua.kauss21@gmail.com>
2026-09-03 16:44:25 +0200

    small fix

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import shutil
 import sys
 
 dir_path_static = "./static"
-dir_path_public = "./doc"
+dir_path_public = "./docs"
 #dir_path_public = "./public"
 dir_path_content = "./content"
 template_path = "./template.html"
